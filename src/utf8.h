93fa9f14de27a75cd52670438f09c75694cdc57d
Lewis Wall <lewis@wall.org>
2019-12-28 11:45:03 -0800

    Add some noop conversions to utf8.h for future compatibility

--- src/utf8.h
+++ src/utf8.h
@@ -4,6 +4,10 @@
 
 std::string to_utf8 (const wchar_t*);
 static inline std::string to_utf8 (const std::wstring& s) { return to_utf8(s.c_str()); }
+static inline const std::string& to_utf8 (const std::string& s) { return s; }
+static inline std::string to_utf8 (const char* s) { return s; }
 
 std::wstring to_utf16 (const char*);
 static inline std::wstring to_utf16 (const std::string& s) { return to_utf16(s.c_str()); }
+static inline const std::wstring& to_utf16 (const std::wstring& s) { return s; }
+static inline std::wstring to_utf16 (const wchar_t* s) { return s; }
