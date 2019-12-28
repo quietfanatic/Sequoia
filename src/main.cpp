81664bfc48b9e3856af20942859a2480129bfa5c
Lewis Wall <lewis@wall.org>
2019-12-27 18:31:07 -0800

    Catch exceptions and show an error message

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 
+#include <exception>
 #include <filesystem>
 #include <fstream>
 #include <string>
@@ -44,39 +45,45 @@ int WINAPI WinMain (
     LPSTR lpCmdLine,
     int nCmdShow
 ) {
-    SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+    try {
+        SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
 
-     // This is so dumb
-    char** argv = __argv;
-    int argc = __argc;
-     // Parse arguments
-    for (int i = 0; i < argc; i++) {
-        LOG("arg", i, argv[i]);
-        std::string arg = argv[i];
-        if (arg.starts_with("--profile-folder=")) {
-            profile_folder = arg.substr(strlen("--profile-folder="));
+         // This is so dumb
+        char** argv = __argv;
+        int argc = __argc;
+         // Parse arguments
+        for (int i = 0; i < argc; i++) {
+            LOG("arg", i, argv[i]);
+            std::string arg = argv[i];
+            if (arg.starts_with("--profile-folder=")) {
+                profile_folder = arg.substr(strlen("--profile-folder="));
+            }
         }
-    }
-     // Figure out profile folder
-    if (profile_folder.empty()) {
-        profile_folder = exe_relative("default-profile");
-    }
-    profile_folder = to_utf8(filesystem::absolute(profile_folder));
-    LOG("Using profile folder:", profile_folder);
-    filesystem::create_directory(profile_folder);
-     // Start
-    load_settings();
-    init_nursery(profile_folder + "/edge-user-data");
-    start_browser();
-     // Run message loop
-    MSG msg;
-    while (GetMessage(&msg, nullptr, 0, 0)) {
-        if (!IsDialogMessage(GetAncestor(msg.hwnd, GA_ROOT), &msg)) {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
+         // Figure out profile folder
+        if (profile_folder.empty()) {
+            profile_folder = exe_relative("default-profile");
         }
+        profile_folder = to_utf8(filesystem::absolute(profile_folder));
+        LOG("Using profile folder:", profile_folder);
+        filesystem::create_directory(profile_folder);
+         // Start
+        load_settings();
+        init_nursery(profile_folder + "/edge-user-data");
+        start_browser();
+         // Run message loop
+        MSG msg;
+        while (GetMessage(&msg, nullptr, 0, 0)) {
+            if (!IsDialogMessage(GetAncestor(msg.hwnd, GA_ROOT), &msg)) {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        return (int)msg.wParam;
     }
-    return (int)msg.wParam;
+    catch (exception& e) {
+        show_string_error(__FILE__, __LINE__, (string("Uncaught exception: ") + e.what()).c_str());
+        return 1;
+    }
 }
 
 void quit () {
