a98d1f2a3dfa550accf930768d39e6672ab061d9
Lewis Wall <lewis@wall.org>
2019-12-30 15:40:56 -0800

    Keep webpage from observing window.chrome.webview.

--- src/res/injection.js
+++ src/res/injection.js
@@ -1,16 +1,23 @@
 "use strict";
+(()=>{
 
-window.addEventListener('auxclick', e=>{
-    console.log(e);
-    if (e.button != 1) return;
-    let $a = e.target.closest('[href]');
+let webview = chrome.webview;
+ // Prevent page from abusing this
+delete chrome.webview;
+
+window.addEventListener('auxclick', event=>{
+    console.log(event);
+    if (event.button != 1) return;
+    let $a = event.target.closest('[href]');
     if ($a === null) return;
     let title = $a.getAttribute("title");
     if (!title) {
         title = $a.innerText.substring(0, 128);
     }
     title = title.replace(/^\n|\n.*/g, "");
-    chrome.webview.postMessage(['new_child_tab', $a.href, title]);
-    e.stopPropagation();
-    e.preventDefault();
+    webview.postMessage(['new_child_tab', $a.href, title]);
+    event.stopPropagation();
+    event.preventDefault();
 });
+
+})();
